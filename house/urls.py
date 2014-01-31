04e2fbd6fa5501228420c92c8904e240ebcad5d2
Hunter Leath <h@hunterleath.com>
2014-01-31 13:52:00 -0500

    Added Rudimentary Editing Functionality

--- house/urls.py
+++ house/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # url(r'^write/$', login_required(StoryCreate.as_view()), name='story_add'),
 
     url(r'^stories/$', StoryList.as_view(), name='story_list'),
-    url(r'^stories/(?P<pk>\d+)/edit/$', login_required(StoryUpdate.as_view()), name='story_update'),
+    # url(r'^stories/(?P<pk>\d+)/edit/$', login_required(StoryUpdate.as_view()), name='story_update'),
     url(r'^stories/(?P<pk>\d+)/delete/$', login_required(StoryDelete.as_view()), name='story_delete'),
 
     # url(r'category/(?P<name>[-a-z0-9_]+)/$', CategoryView.as_view(), name='category_view'),
@@ -24,7 +24,7 @@ urlpatterns = patterns('',
 
     url(r'^account/stories/$', login_required(AccountStories.as_view()), name='admin_story_list'),
     url(r'^account/stories/write/$', login_required(StoryCreate.as_view()), name='story_add'),
-    url(r'^account/stories/(?P<pk>\d+)/edit/$', login_required(AccountDashboard.as_view()), name='story_edit'),
+    url(r'^account/stories/(?P<pk>\d+)/edit/$', login_required(StoryUpdate.as_view()), name='story_edit'),
     url(r'^account/stories/(?P<pk>\d+)/delete/$', login_required(AccountDashboard.as_view()), name='story_delete'),
 
     url(r'^(?P<user>[-a-z0-9_]+)/(?P<slug>[-a-z0-9_]+)/$', StoryView.as_view(), name="story_view"),
